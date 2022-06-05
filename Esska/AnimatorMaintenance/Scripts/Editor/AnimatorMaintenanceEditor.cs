0d8bb49ec1905ba54461744ac2ca1fc62752b579
Esska <esska@kanivels.com>
2022-06-05 16:29:09 +0200

    Initial release

---
+++ Esska/AnimatorMaintenance/Scripts/Editor/AnimatorMaintenanceEditor.cs
@@ -0,0 +1,271 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Esska.AnimatorMaintenance {
+
+    [CustomEditor(typeof(AnimatorMaintenance))]
+    public class AnimatorMainteneanceEditor : Editor {
+
+        static bool confirmBackup;
+
+        AnimatorMaintenance maintenance;
+
+        void OnEnable() {
+            maintenance = (AnimatorMaintenance)target;
+        }
+
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            if (!maintenance.isActiveAndEnabled)
+                return;
+
+            GUILayout.Space(10f);
+
+            if (Application.isPlaying) {
+                EditorGUILayout.HelpBox("Controls are disabled during play mode", MessageType.Info, true);
+                return;
+            }
+
+            if (maintenance.controller == null) {
+                EditorGUILayout.HelpBox("Please select an Animator Controller", MessageType.Info, true);
+                return;
+            }
+
+            RenderGUI((AnimatorController)maintenance.controller);
+        }
+
+        public static void RenderGUI(AnimatorController controller) {
+
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button("List Content By Type"))
+                    ListContentByType(controller);
+
+                if (GUILayout.Button("List Unused Content"))
+                    AnalyzeRemoveContent(controller);
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button("Show All Content"))
+                    ShowContent(controller);
+
+                if (GUILayout.Button("Show Unused Content"))
+                    ShowContent(controller, true);
+
+                if (GUILayout.Button("Hide Content"))
+                    HideContent(controller);
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(10f);
+
+            confirmBackup = GUILayout.Toggle(confirmBackup, "I confirm, I've made a BACKUP of the controller!");
+
+            GUILayout.Space(5f);
+
+            GUI.enabled = confirmBackup;
+
+            if (GUILayout.Button("Remove Unused Content")) {
+                AnalyzeRemoveContent(controller, true);
+                confirmBackup = false;
+            }
+
+            GUI.enabled = true;
+        }
+
+        public static void ListContentByType(AnimatorController controller) {
+            Object[] objects = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(controller));
+            Dictionary<System.Type, int> results = new Dictionary<System.Type, int>();
+            int nullCount = 0;
+
+            Debug.Log(string.Format("<b>List of content by type ({0}):</b>", controller.name));
+
+            foreach (var item in objects) {
+
+                if (item != null) {
+                    System.Type type = item.GetType();
+
+                    if (!results.ContainsKey(type))
+                        results.Add(type, 0);
+
+                    results[type]++;
+                }
+                else {
+                    nullCount++;
+                }
+            }
+
+            foreach (var item in results) {
+                Debug.Log(string.Format("{0}: {1}", item.Key, item.Value));
+            }
+
+            if (nullCount > 0)
+                Debug.Log(string.Format("Null: {0}", nullCount));
+
+            Debug.Log(string.Format("<b>Total count: {0}</b>", objects.Length));
+        }
+
+        public static void AnalyzeRemoveContent(AnimatorController controller, bool removeUnused = false) {
+            Object[] objects = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(controller));
+            string suffix = removeUnused ? "removed" : "not used";
+            int counter = 0;
+
+            if (!removeUnused)
+                Debug.Log(string.Format("<b>List of unused content ({0}):</b>", controller.name));
+            else
+                Debug.Log(string.Format("<b>Remove unused content ({0}):</b>", controller.name));
+
+            foreach (var item in objects) {
+
+                if (item != null) {
+
+                    if (item is AnimatorStateMachine) {
+                        AnimatorStateMachine stateMachine = (AnimatorStateMachine)item;
+
+                        if (!controller.IsStateMachineUsed(stateMachine)) {
+
+                            if (removeUnused)
+                                AssetDatabase.RemoveObjectFromAsset(item);
+
+                            Debug.Log(string.Format("StateMachine '{0}' {1}", stateMachine.name, suffix));
+                            counter++;
+                        }
+                    }
+                    else if (item is AnimatorState) {
+                        AnimatorState state = (AnimatorState)item;
+
+                        if (!controller.IsStateUsed(state)) {
+
+                            if (removeUnused)
+                                AssetDatabase.RemoveObjectFromAsset(item);
+
+                            Debug.Log(string.Format("State '{0}' {1}", state.name, suffix));
+                            counter++;
+                        }
+
+                    }
+                    else if (item is BlendTree) {
+                        BlendTree blendTree = (BlendTree)item;
+
+                        if (!controller.IsBlendTreeUsed(blendTree)) {
+
+                            if (removeUnused)
+                                AssetDatabase.RemoveObjectFromAsset(item);
+
+                            Debug.Log(string.Format("BlendTree '{0}' {1}", blendTree.name, suffix));
+                            counter++;
+                        }
+
+                    }
+                    else if (item is AnimatorStateTransition) {
+                        AnimatorStateTransition transition = (AnimatorStateTransition)item;
+
+                        if (!controller.IsStateTransitionUsed(transition)) {
+
+                            if (removeUnused)
+                                AssetDatabase.RemoveObjectFromAsset(item);
+
+                            Debug.Log(string.Format("StateTransition {0}", suffix));
+                            counter++;
+                        }
+
+                    }
+                    else if (item is AnimatorTransition) {
+                        AnimatorTransition transition = (AnimatorTransition)item;
+
+                        if (!((transition.destinationStateMachine != null && controller.IsStateMachineUsed(transition.destinationStateMachine)) || (transition.destinationState != null && controller.IsStateUsed(transition.destinationState)))) {
+
+                            if (removeUnused)
+                                AssetDatabase.RemoveObjectFromAsset(item);
+
+                            Debug.Log(string.Format("Transition {0}", suffix));
+                            counter++;
+                        }
+
+                    }
+                    else if (item is StateMachineBehaviour) {
+                        StateMachineBehaviour behaviour = (StateMachineBehaviour)item;
+
+                        if (!controller.IsStateMachineBehaviourUsed(behaviour)) {
+
+                            if (removeUnused)
+                                AssetDatabase.RemoveObjectFromAsset(item);
+
+                            Debug.Log(string.Format("StateMachineBehavior {0} {1}", item.GetType(), suffix));
+                            counter++;
+                        }
+
+                    }
+                }
+            }
+
+            if (removeUnused)
+                AssetDatabase.SaveAssets();
+
+            if (!removeUnused)
+                Debug.Log(string.Format("<b>Found {0} unused objects</b>", counter));
+            else
+                Debug.Log(string.Format("<b>Removed {0} unused objects</b>", counter));
+        }
+
+        public static void ShowContent(AnimatorController controller, bool showUnusedObjectsOnly = false) {
+            Object[] objects = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(controller));
+
+            foreach (var item in objects) {
+
+                if (item != null && !(item is AnimatorController)) {
+                    bool show = !showUnusedObjectsOnly;
+
+                    if (showUnusedObjectsOnly) {
+
+                        if (item is AnimatorStateMachine && !controller.IsStateMachineUsed((AnimatorStateMachine)item))
+                            show = true;
+                        else if (item is AnimatorState && !controller.IsStateUsed((AnimatorState)item))
+                            show = true;
+                        else if (item is BlendTree && !controller.IsBlendTreeUsed((BlendTree)item))
+                            show = true;
+                        else if (item is AnimatorStateTransition && !controller.IsStateTransitionUsed((AnimatorStateTransition)item))
+                            show = true;
+                        else if (item is AnimatorTransition) {
+                            AnimatorTransition transition = (AnimatorTransition)item;
+
+                            if (!((transition.destinationStateMachine != null && controller.IsStateMachineUsed(transition.destinationStateMachine)) || (transition.destinationState != null && controller.IsStateUsed(transition.destinationState))))
+                                show = true;
+                        }
+                        else if (item is StateMachineBehaviour && !controller.IsStateMachineBehaviourUsed((StateMachineBehaviour)item))
+                            show = true;
+                    }
+
+                    if (show)
+                        item.hideFlags = HideFlags.None;
+                    else if (showUnusedObjectsOnly)
+                        item.hideFlags = HideFlags.HideInHierarchy;
+
+                    EditorUtility.SetDirty(item);
+                }
+            }
+
+            AssetDatabase.SaveAssets();
+            EditorGUIUtility.PingObject(controller);
+        }
+
+        public static void HideContent(AnimatorController controller) {
+            Object[] objects = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(controller));
+
+            foreach (var item in objects) {
+
+                if (item != null && !(item is AnimatorController)) {
+                    item.hideFlags = HideFlags.HideInHierarchy;
+                    EditorUtility.SetDirty(item);
+                }
+            }
+
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
---
+++ Esska/AnimatorMaintenance/Scripts/Editor/AnimatorMaintenanceExtensions.cs
@@ -0,0 +1,190 @@
+﻿using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Esska.AnimatorMaintenance {
+
+    public static class AnimatorControllerExtensions {
+
+        public static bool IsStateMachineUsed(this AnimatorController controller, AnimatorStateMachine findStateMachine) {
+
+            foreach (var layer in controller.layers) {
+
+                if (layer.stateMachine == findStateMachine)
+                    return true;
+
+                if (layer.stateMachine.IsStateMachineUsed(findStateMachine))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsStateUsed(this AnimatorController controller, AnimatorState findState) {
+
+            foreach (var layer in controller.layers) {
+
+                if (layer.stateMachine.IsStateUsed(findState))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsBlendTreeUsed(this AnimatorController controller, BlendTree findBlendTree) {
+
+            foreach (var layer in controller.layers) {
+
+                if (layer.stateMachine.IsBlendTreeUsed(findBlendTree))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsStateTransitionUsed(this AnimatorController controller, AnimatorStateTransition findStateTransition) {
+
+            foreach (var layer in controller.layers) {
+
+                if (layer.stateMachine.IsStateTransitionUsed(findStateTransition))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsStateMachineBehaviourUsed(this AnimatorController controller, StateMachineBehaviour findStateMachineBehaviour) {
+
+            foreach (var layer in controller.layers) {
+
+                if (layer.stateMachine.IsStateMachineBehaviourUsed(findStateMachineBehaviour))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    public static partial class AnimatorStateMachineExtensions {
+
+        public static bool IsStateMachineUsed(this AnimatorStateMachine stateMachine, AnimatorStateMachine findStateMachine) {
+
+            foreach (var subStateMachine in stateMachine.stateMachines) {
+
+                if (subStateMachine.stateMachine == findStateMachine)
+                    return true;
+
+                if (subStateMachine.stateMachine.IsStateMachineUsed(findStateMachine))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsStateUsed(this AnimatorStateMachine stateMachine, AnimatorState findState) {
+
+            foreach (var state in stateMachine.states) {
+
+                if (state.state == findState)
+                    return true;
+            }
+
+            foreach (var subStateMachine in stateMachine.stateMachines) {
+
+                if (subStateMachine.stateMachine.IsStateUsed(findState))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsBlendTreeUsed(this AnimatorStateMachine stateMachine, BlendTree findBlendTree) {
+
+            foreach (var state in stateMachine.states) {
+
+                if (state.state.motion == findBlendTree)
+                    return true;
+
+                if (state.state.motion is BlendTree)
+                    return ((BlendTree)state.state.motion).IsBlendTreeUsed(findBlendTree);
+            }
+
+            foreach (var subStateMachine in stateMachine.stateMachines) {
+
+                if (subStateMachine.stateMachine.IsBlendTreeUsed(findBlendTree))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsStateTransitionUsed(this AnimatorStateMachine stateMachine, AnimatorStateTransition findStateTransition) {
+
+            foreach (var transitions in stateMachine.entryTransitions) {
+
+                if (transitions == findStateTransition)
+                    return true;
+            }
+
+            foreach (var transitions in stateMachine.anyStateTransitions) {
+
+                if (transitions == findStateTransition)
+                    return true;
+            }
+
+            foreach (var state in stateMachine.states) {
+
+                foreach (var transitions in state.state.transitions) {
+
+                    if (transitions == findStateTransition)
+                        return true;
+                }
+            }
+
+            foreach (var subStateMachine in stateMachine.stateMachines) {
+
+                if (subStateMachine.stateMachine.IsStateTransitionUsed(findStateTransition))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsStateMachineBehaviourUsed(this AnimatorStateMachine stateMachine, StateMachineBehaviour findStateMachineBehaviour) {
+
+            foreach (var state in stateMachine.states) {
+
+                foreach (var behaviour in state.state.behaviours) {
+
+                    if (behaviour == findStateMachineBehaviour)
+                        return true;
+                }
+            }
+
+            foreach (var subStateMachine in stateMachine.stateMachines) {
+
+                if (subStateMachine.stateMachine.IsStateMachineBehaviourUsed(findStateMachineBehaviour))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    public static partial class BlendTreeExtensions {
+
+        public static bool IsBlendTreeUsed(this BlendTree blendTree, BlendTree findBlendTree) {
+
+            foreach (var item in blendTree.children) {
+
+                if (item.motion == findBlendTree)
+                    return true;
+
+                if (item.motion is BlendTree)
+                    return ((BlendTree)item.motion).IsBlendTreeUsed(findBlendTree);
+            }
+
+            return false;
+        }
+    }
+
+}
